07b66ca99e36c92e0e25bfc1c4481806fca4374d
cvncerboy <isfanbimantara@gmail.com>
2021-11-25 19:37:19 +0700

    new commit

--- is-prime-num.js
+++ is-prime-num.js
@@ -27,4 +27,3 @@ console.log(isPrime(23)) // 23 adalah bilangan prima
 console.log(isPrime(7)) // 23 adalah bilangan prima
 console.log(isPrime(0)) // harus memasukan angka mulai dari 2
 console.log(isPrime(1)) // harus memasukan angka mulai dari 2
-console.log(isPrime(-2))
